32ce6e0c1a1dbc8f3b5c4a3ef1ccb11edaf758d0
devsam19 <sanjat.mishra@outlook.com>
2018-11-08 15:25:03 -0800

    Added Dashboard

--- index.js
+++ index.js
@@ -2,26 +2,37 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
-// set view engine
+var router = express.Router();
+//set path
 
+var path = __dirname + '/views/';
+
+// set view engine
+/* Use Handlebars later
 var handlebars = require('express3-handlebars').create({defaultLayout : 'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-
+*/
 // set port 3000
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 // set routes
 
+router.use(function (req,res,next) {
+    console.log("/" + req.method);
+    next();
+  });
+
 app.get('/', function(req,res){
-    res.render('home');
+    res.sendFile(path +  'dashboard.html');
 });
 
-app.use(function(req,res){
-    res.status(404);
-    res.render('404');
-});
+
 
 app.listen(app.get('port'), function(){
     console.log('Starting express on localhost : ' + app.get('port') + '; Press Ctrl-C to terminate');
